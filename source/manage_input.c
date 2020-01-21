7e4c2cd32fe6b5fe4621953d41c50c7841f84b6d
Justine Fricou <jfricou@justine.fricou>
2020-01-21 12:47:01 +0100

    ADDED manage_input.c, my_tools.c : added exit command

---
+++ source/manage_input.c
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2020
+** manage_input.c
+** File description:
+** mysh (minishell1) : gets the input and calls the right function
+*/
+
+#include "mysh.h"
+
+int get_input(char **input)
+{
+    ssize_t getline_size = 0;
+    size_t n = 0;
+    int length = 0;
+
+    getline_size = getline(input, &n, stdin);
+    if (getline_size < 0)
+        return (84);
+    for (; (*input)[length] != '\n' && (*input)[length] != 0; length++);
+    (*input)[length] = 0;
+    return (0);
+}
+
+int handle_input(char *input, int *go_on)
+{
+    int index = 0;
+
+    for (; input[index] == ' '; index++);
+    if (my_strcmp(&(input[index]), "exit") == 0) {
+        index += 5;
+        for (; input[index] == ' '; index++);
+        if (input[index] == 0)
+            *go_on = 0;
+    }
+    return (0);
+}
---
+++ source/my_tools.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2020
+** my_tools.c
+** File description:
+** mysh (minishell1) : several tools
+*/
+
+#include "mysh.h"
+
+int my_strcmp(char *s1, char *s2)
+{
+    int i = 0;
+
+    for (; s1[i] != 0 && s2[i] != 0 && s1[i] != ' ' && s2[i] != ' '; i++) {
+        if (s1[i] != s2[i])
+            return (s1[i] - s2[i]);
+    }
+    return (0);
+}
