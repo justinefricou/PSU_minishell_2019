8e44a9a273c57f40cea06a83fda98fb9daaf279a
Justine Fricou <jfricou@justine.fricou>
2020-01-25 13:45:52 +0100

    RENAMED manage_input.c -> main_loop.c

--- source/main_loop.c
+++ source/main_loop.c
@@ -1,11 +1,30 @@
 /*
 ** EPITECH PROJECT, 2020
-** manage_input.c
+** main_loop.c
 ** File description:
-** mysh (minishell1) : gets the input and calls the right function
+** mysh (minishell1) : gets the inputs and calls the right functions
 */
 
 #include "mysh.h"
+
+int main_loop(env_var *env_vars)
+{
+    char *input = NULL;
+    int go_on = 1;
+
+    signal(SIGINT, SIG_IGN);
+    while (go_on) {
+        write(1, "$> ", 3);
+        if (get_input(&input) == 84)
+            return (84);
+        if (handle_input(input, env_vars, &go_on) == 84) {
+            free(input);
+            return (84);
+        }
+        free(input);
+    }
+    return (0);
+}
 
 int get_input(char **input)
 {
@@ -21,14 +40,14 @@ int get_input(char **input)
     return (0);
 }
 
-int handle_input(char *input, char **env, int *go_on)
+int handle_input(char *input, env_var *env_vars, int *go_on)
 {
     for (; *input == ' '; input++);
     if (is_command("exit", input))
         *go_on = 0;
     if (is_command("cd", input))
         launch_cd(input);
-    else if (handle_env_related_builtins(input, env) == 84)
+    else if (handle_env_related_builtins(input, env_vars) == 84)
         return (84);
     return (0);
 }
