600ed3f5a520de09f112eeba86220df6f80164c4
Justine Fricou <jfricou@justine.fricou>
2020-01-26 13:20:04 +0100

    RENAMED env_related_builtins.c -> handle_env_related_builtins.c

---
+++ source/handle_env_related_builtins.c
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2020
+** handle_env_related_builtins.c
+** File description:
+** mysh (minishell1) : launches env, setenv and unsetenv builtins
+*/
+
+#include "mysh.h"
+
+int handle_env_related_builtins(char *input, env_var *env_vars)
+{
+    if (is_command("env", input))
+        launch_env(env_vars);
+    if (is_command("setenv", input)) {
+        if (launch_setenv(env_vars, input) == 84)
+            return (84);
+    } /*else if (is_command("unsetenv", input)) {
+        if (lauch_unsetenv() == 84)
+            return (84);
+    }*/
+    return (0);
+}
+
+void launch_env(env_var *env_vars)
+{
+    while (env_vars != NULL) {
+        my_put_str(env_vars->name);
+        write(1, "=", 1);
+        my_put_str(env_vars->value);
+        write(1, "\n", 1);
+        env_vars = env_vars->next;
+    }
+}
